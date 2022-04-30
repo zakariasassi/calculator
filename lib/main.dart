6f9d05ef0421d1476bc592ebafb826c228d1554b
zakaria sassi <king.herts.zeko@gmail.com>
2022-04-30 05:09:04 +0200

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -52,15 +52,48 @@ class _MyHomePageState extends State<MyHomePage> {
         Expanded(
             flex: 2,
             child: Container(
-              child: Center(
-                child: myButtuns(
-                  textButton: "0",
-                  color: Colors.pinkAccent.shade400,
-                  textColor: Colors.white,
-                ),
-              ),
-            )),
+                child: GridView.builder(
+                    itemCount: buttns.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4),
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == 0) {
+                        return myButtuns(
+                          textButton: buttns[index],
+                          color: Color.fromARGB(255, 255, 26, 26),
+                          textColor: Color.fromARGB(255, 255, 255, 255),
+                        );
+                      } else if (index == 1) {
+                        return myButtuns(
+                          textButton: buttns[index],
+                          color: Color.fromARGB(255, 26, 255, 76),
+                          textColor: Color.fromARGB(255, 255, 255, 255),
+                        );
+                      } else {
+                        return myButtuns(
+                          textButton: buttns[index],
+                          color: isTorF(buttns[index])
+                              ? Colors.deepPurple
+                              : Colors.deepPurple.shade50,
+                          textColor: isTorF(buttns[index])
+                              ? Colors.white
+                              : Colors.deepPurple,
+                        );
+                      }
+                    }))),
       ]),
     );
+  }
+
+  bool isTorF(String string) {
+    if (string == "%" ||
+        string == "/" ||
+        string == "x" ||
+        string == "-" ||
+        string == "+" ||
+        string == "=") {
+      return true;
+    }
+    return false;
   }
 }
